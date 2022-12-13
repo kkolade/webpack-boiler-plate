b4ae9dffc239b2523e32dd7a576ac2e525929960
Kola Kolade <kolakolade@gmail.com>
2022-12-13 18:41:36 +0100

    Added html and CSS support along with devserver

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: path.resolve(__dirname, "index.html")
+    })
+  ],
+  devServer: {
+    static: './dist',
+  },
+  module : {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
